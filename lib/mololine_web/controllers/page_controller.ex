3e16848256d264b13b2fb545746fb8a837bbbebe
BLUECALF <leonardkiptala@gmail.com>
2022-11-05 20:08:31 +0300

    FIX:: - made customers  visit their page 5/11/22

--- lib/mololine_web/controllers/page_controller.ex
+++ lib/mololine_web/controllers/page_controller.ex
@@ -1,11 +1,13 @@
 defmodule MololineWeb.PageController do
   use MololineWeb, :controller
 
+  alias Mololine.Towns
   def index(conn, _params) do
     role = conn.assigns.current_user.role
+    towns = Towns.list_towns()
     case role do
       "customer" -> #render(conn, "customer.html")
-        render(conn, "customer.html")
+        render(conn, "customer.html",towns: towns)
       "admin" -> render(conn, "admin.html")
       "driver" -> render(conn, "driver.html")
       "accountant" -> render(conn, "accountant.html")
